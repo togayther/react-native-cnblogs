32b00b1c23fc1d3809e6feafb77e37a4caa1e8c5
kanging <sleepsleepsleep@foxmail.com>
2016-05-26 17:43:53 +0800

    again and again

--- source/component/htmlRender.js
+++ source/component/htmlRender.js
@@ -32,8 +32,11 @@ class HtmlRender extends Component {
 	    let codeLines = codeText.split('\n');
 	    let codeLen = codeLines.length;
 	    return codeLines.map(function (line, index, arr) {
-	        if (index == (codeLen - 1)) return '';
+	        if (index == (codeLen - 1)) return null;
 	        if (line == '') line = '\n';
+
+	        line = _.replace(line, '&#xD;', '');
+
 	        let lineNum = index + 1;
 	        return (
 	            <View key={'codeRow'+index} style={HtmlRenderStyle.codeRow}>
@@ -71,48 +74,72 @@ class HtmlRender extends Component {
 		});
 	}
 
-	renderNode(node, index, list) {
-		if(node.name && node.name == 'img'){
-			const imageUri = node.attribs.src;
-			const imageId = _.uniqueId('image_');
-			return (
-				<Image
-					key={ imageId }
-					ref={ view=>this.images[imageId] = view }
-					style={ contentStyles.img }
-					source={{uri:imageUri}}
-					onLoadEnd={ this.onImageLoadEnd.bind(this, imageUri, imageId) }
-				/>
-			)
+	getNodeCodeText(node, codeText){
+		if(node.type == 'text'){
+        	codeText = codeText + node.data;
+		}
+		if(node.name && node.children && node.children.length){
+			node.children.map((child)=>{
+				return this.getNodeCodeText(child, codeText);
+			});
 		}
 
+		return codeText;
+	}
 
-		if(node.name && node.name == 'pre'){
-			if (node.children && node.children.length > 0) {
-				let codeText = '';
-				node.children.forEach(function (code) {
-					if(code.data){
-	                	codeText = codeText + code.data;
-					}
-					if(code.name == "span"){
-						let codeChild = code.children[0];
-						if (codeChild.data) {
-							codeText = codeText + codeChild.data;
-						}
-					}
-	            });
+	renderNode(node, index, list) {
+		if(node.type == 'tag'){
+			if(node.name == "img" && node.attribs && node.attribs.src){
+				let imageUri = node.attribs.src;
+				let imageId = _.uniqueId('image_');
+				return (
+					<Image
+						key={ imageId }
+						ref={ view=>this.images[imageId] = view }
+						style={ contentStyles.img }
+						source={{uri:imageUri}}
+						onLoadEnd={ this.onImageLoadEnd.bind(this, imageUri, imageId) }
+					/>
+				);
+			}
 
-	            const codeId = _.uniqueId('code_');
+			if(node.name == "code"){
+				let codeId = _.uniqueId('code_');
+				let codeText = "";
+                codeText = this.getNodeCodeText(node, codeText);
+
+                console.info("renderNode code");
+				console.info(codeText);
+
+                return (
+                    <ScrollView
+                    	key= { codeId }
+                        style={HtmlRenderStyle.codeScrollView}
+                        horizontal={true}>
+                        <View style={HtmlRenderStyle.codeWrapper}>
+                            { this.renderCodeBlock(codeText) }
+                        </View>
+                    </ScrollView>
+                );
+			}
+
+			if(node.name == "div" && node.attribs && node.attribs.class && node.attribs.class=="cnblogs_code"){
+				let codeId = _.uniqueId('cnblogs_code_');
+				let codeText = "";
+                codeText = this.getNodeCodeText(node, codeText);
+
+				console.info("renderNode cnblogs_code");
+				console.info(codeText);
 
 	            return (
-	            	<View
+	            	<ScrollView
 	                    key= {codeId}
 	                    horizontal={true}
 	                    style={HtmlRenderStyle.codeScrollView}>
 	                    <View style={HtmlRenderStyle.codeWrapper}>
 	                        { this.renderCodeBlock(codeText) }
 	                    </View>
-	                </View>
+	                </ScrollView>
 	           );
 			}
 		}
