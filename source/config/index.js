cb346bdf396f07d799970f88f3c4896e56814ee8
togayther <sleepsleepsleep@foxmail.com>
2016-11-01 17:18:15 +0800

    always forgot update config

--- source/config/index.js
+++ source/config/index.js
@@ -4,7 +4,7 @@ export default {
 		name:'博客园',
 		descr:'开发者的网上家园',
 		site:'www.cnblogs.com',
-		version: 'v3.0.0',
+		version: 'v3.1.0',
 		copyright: '©2016 powered by react-native',
 		registerUri: 'https://passport.cnblogs.com/register.aspx',
 		declare: '博客园创立于2004年1月，是一个面向开发者的知识分享社区。自创建以来，博客园一直致力并专注于为开发者打造一个纯净的技术交流社区，推动并帮助开发者通过互联网分享知识，从而让更多开发者从中受益。博客园的使命是帮助开发者用代码改变世界。'
@@ -30,9 +30,9 @@ export const postCategory = {
 };
 
 export const authData = {
-	pubKey : "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCp0wHYbg/NOPO3nzMD3dndwS0MccuMeXCHgVlGOoYyFwLdS24Im2e7YyhB0wrUsyYf0/nhzCzBK8ZC9eCWqd0aHbdgOQT6CuFQBMjbyGYvlVYU2ZP7kG9Ft6YV6oc9ambuO7nPZh+bvXH0zDKfi02prknrScAKC0XhadTHT3Al0QIDAQAB\n-----END PUBLIC KEY-----",
- 	clientId: "cdfb6ec8-e78d-4c70-82df-7b1651a98808",
- 	clientSecret: "TFgkwiEVDBGZwPncPL9b5a9_z7E2pipUPFHo9OWIeOXkGTImxr_-LQBMw9_gQLX94Faqkbs9VbL_CKk-"
+	pubKey : "", //向博客园申请后填入
+ 	clientId: "",//向博客园申请后填入
+ 	clientSecret: ""//向博客园申请后填入
 };
 
 export const pageSize = 10;
